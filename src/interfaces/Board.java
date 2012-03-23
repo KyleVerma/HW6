1c0ed9d70ee830dea34150bdaccb44fe8f74b0c5
Bradley M. Charna <bcharna@andrew.cmu.edu>
2012-03-23 16:10:54 -0400

    started Board interface

--- src/interfaces/Board.java
+++ src/interfaces/Board.java
@@ -1,5 +1,17 @@
 package interfaces;
 
-public interface Board {
+import java.util.Hashtable;
 
+public interface Board {
+	
+	//Hashtable<String, Object> cells; ivars dont go in interface
+	
+	/**
+	 * 
+	 * @return array of hash tables containing keys like enabled and color
+	 */
+	Hashtable<String, Object>[] getCells();
+	
+	
+	
 }
